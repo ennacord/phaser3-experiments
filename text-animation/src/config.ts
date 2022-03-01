616285f406841cf434d1ac591782dcdb9d3ccf81
Johannes Choo <jhanschoo@gmail.com>
2022-03-01 10:15:47 +0800

    implement basic appearance in text-animation

--- text-animation/src/config.ts
+++ text-animation/src/config.ts
@@ -1,12 +1,13 @@
 import Phaser from 'phaser';
+import { WINDOW_WIDTH, WINDOW_HEIGHT } from './constants';
 
 export default {
   type: Phaser.AUTO,
   parent: 'game',
   backgroundColor: '#33A5E7',
   scale: {
-    width: 800,
-    height: 600,
+    width: WINDOW_WIDTH,
+    height: WINDOW_HEIGHT,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH
   }
---
+++ text-animation/src/constants.ts
@@ -0,0 +1,2 @@
+export const WINDOW_WIDTH = 800;
+export const WINDOW_HEIGHT = 600;
